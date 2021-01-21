01523cab0d5b121d22dcef5b92b25dba4b4f166d
Lo Hedengran <lohedengran@gmail.com>
2021-01-21 11:01:33 +0100

    Initial commit

---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Addera(2, 5);
+            Console.WriteLine(sum);
+
+            double moms = Moms(100);
+            Console.WriteLine(moms);
+
+            double moms2 = Moms(100, 0.12);
+            Console.WriteLine(moms2);
+        }
+        static int Addera(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static double Moms(double num)
+        {
+            return num * 0.25;
+        }
+
+        static double Moms(double num, double moms)
+        {
+            return num * moms;
+        }
+    }
+}
---
+++ Week1Day4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Week1Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wholeNumber = 8;
+            Console.WriteLine(wholeNumber);
+            int returnValue = AddTwo(wholeNumber);
+            Console.WriteLine(returnValue);
+            Console.WriteLine(wholeNumber);
+            //SomeMethod();
+            //int i = SomeMethodWithReturnValue();
+            //string returnString = GetAString();
+            //TakeAMessageAndShout("Academy"); - argument i form av en literal
+            Console.Write("Ange en sträng: ");
+            string message = Console.ReadLine();
+            TakeAMessageAndShout(message); //argument i form av en variabel
+
+            Console.Write("Ange en sträng: ");
+            TakeAMessageAndShout(Console.ReadLine()); // argument i form av ett metodanrop
+        }
+        static void SomeMethod() // enklast metod
+        {
+            Console.WriteLine("Academy!");
+            Console.WriteLine("2021"); 
+        }
+
+        static int SomeMethodWithReturnValue() // enklast metod
+        {
+            bool done = true;
+            if (done)
+            {
+                return 1;
+            } else
+            return 0;
+        }
+
+        static string GetAString()
+        {
+            return Console.ReadLine();
+        }
+
+        static void TakeAMessageAndShout(string text)
+        {
+            Console.WriteLine(text.ToUpper());
+        }
+
+        static int AddTwo(int wholeNumber)
+        {
+            wholeNumber = wholeNumber + 2;
+            return wholeNumber;
+        }
+    }
+}
